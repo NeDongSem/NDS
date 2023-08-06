f775d4ed3a53e5f694972fe18b02aba095cdd9ab
NeDongSem <riverhun2@gmail.com>
2023-08-07 08:45:56 +0900

    카메라 터치로 이동, 줌인 줌아웃, 세로모드 추가(세로모드는 테스트용, 변경 가능)

--- NeDongSem/Assets/0_NeDongSem/Script/Mng/InputMng.cs
+++ NeDongSem/Assets/0_NeDongSem/Script/Mng/InputMng.cs
@@ -23,22 +23,22 @@ public class InputMng : MonoBehaviour
     }
 
     Touch[] m_TouchArray;
-    Vector2[] m_v2TouchArray;
     public Camera m_MainCamera;
     public float m_CameraMoveSpeed = 3f;
     public float m_CameraZoomSpeed = 2f;
+    public Vector2 m_CameraMoveMinMaxPosX = Vector2.zero;
+    public Vector2 m_CameraMoveMinMaxPosY = Vector2.zero;
     public Vector2 m_CameraFovMinMax = Vector2.zero;
     Vector2 m_v2TouchCenterPos = Vector2.zero;
     Vector2 m_v2TouchPreCenterPos = Vector2.zero;
     Vector2 m_v2TouchMovePos = Vector2.zero;
     float m_fTouchMag;
     float m_fTouchPreMag;
-    float m_fCameraMove;
+    float m_fCameraZoom;
 
     private void Init()
     {
         m_TouchArray = new Touch[2];
-        m_v2TouchArray = new Vector2[2];
         m_fTouchPreMag = 0f;
     }
 
@@ -59,7 +59,6 @@ public class InputMng : MonoBehaviour
             }
             else
             {
-                TouchCalculate();
                 TouchCamera();
             }
         }
@@ -77,14 +76,6 @@ public class InputMng : MonoBehaviour
         }
     }
 
-    private void TouchCalculate()
-    {
-        for (int i = 0; i < m_v2TouchArray.Length; ++i)
-        {
-            m_v2TouchArray[i] = m_TouchArray[i].position - m_TouchArray[i].deltaPosition;
-        }
-    }
-
     private void TouchUI()
     {
 
@@ -98,8 +89,15 @@ public class InputMng : MonoBehaviour
 
     private void TouchCameraMove()
     {
-        m_v2TouchCenterPos.x = (m_v2TouchArray[0].x + m_v2TouchArray[1].x) * 0.5f;
-        m_v2TouchCenterPos.y = (m_v2TouchArray[0].y + m_v2TouchArray[1].y) * 0.5f;
+        if(m_v2TouchPreCenterPos.x <= 0f && m_v2TouchPreCenterPos.y <= 0f)
+        {
+            m_v2TouchPreCenterPos.x = (m_TouchArray[0].position.x + m_TouchArray[1].position.x) * 0.5f;
+            m_v2TouchPreCenterPos.y = (m_TouchArray[0].position.y + m_TouchArray[1].position.y) * 0.5f;
+            return;
+        }
+
+        m_v2TouchCenterPos.x = (m_TouchArray[0].position.x + m_TouchArray[1].position.x) * 0.5f;
+        m_v2TouchCenterPos.y = (m_TouchArray[0].position.y + m_TouchArray[1].position.y) * 0.5f;
 
         m_v2TouchMovePos.x = m_v2TouchPreCenterPos.x - m_v2TouchCenterPos.x;
         m_v2TouchMovePos.y = m_v2TouchPreCenterPos.y - m_v2TouchCenterPos.y;
@@ -107,6 +105,25 @@ public class InputMng : MonoBehaviour
         Vector3 v3MainCameraPos = m_MainCamera.transform.position;
         v3MainCameraPos.x += m_v2TouchMovePos.x * m_CameraMoveSpeed * Time.deltaTime;
         v3MainCameraPos.y += m_v2TouchMovePos.y * m_CameraMoveSpeed * Time.deltaTime;
+
+        if(v3MainCameraPos.x < m_CameraMoveMinMaxPosX.x)
+        {
+            v3MainCameraPos.x = m_CameraMoveMinMaxPosX.x;
+        }
+        else if(v3MainCameraPos.x > m_CameraMoveMinMaxPosX.y)
+        {
+            v3MainCameraPos.x = m_CameraMoveMinMaxPosX.y;
+        }
+
+        if (v3MainCameraPos.y < m_CameraMoveMinMaxPosY.x)
+        {
+            v3MainCameraPos.y = m_CameraMoveMinMaxPosY.x;
+        }
+        else if (v3MainCameraPos.y > m_CameraMoveMinMaxPosY.y)
+        {
+            v3MainCameraPos.y = m_CameraMoveMinMaxPosY.y;
+        }
+
         m_MainCamera.transform.position = v3MainCameraPos;
 
         m_v2TouchPreCenterPos = m_v2TouchCenterPos;
@@ -114,18 +131,24 @@ public class InputMng : MonoBehaviour
 
     private void TouchCameraZoom()
     {
-        m_fTouchMag = (m_v2TouchArray[0] - m_v2TouchArray[1]).magnitude;
+        if(m_fTouchPreMag <= 0f)
+        {
+            m_fTouchPreMag = (m_TouchArray[0].position - m_TouchArray[1].position).magnitude;
+            return;
+        }
 
-        m_fCameraMove = m_fTouchPreMag - m_fTouchMag;
+        m_fTouchMag = (m_TouchArray[0].position - m_TouchArray[1].position).magnitude;
+
+        m_fCameraZoom = m_fTouchPreMag - m_fTouchMag;
 
         if (m_MainCamera.orthographic)
         {
-            m_MainCamera.orthographicSize += m_fCameraMove * m_CameraZoomSpeed * Time.deltaTime;
+            m_MainCamera.orthographicSize += m_fCameraZoom * m_CameraZoomSpeed * Time.deltaTime;
             m_MainCamera.orthographicSize = Mathf.Max(m_MainCamera.orthographicSize, m_CameraFovMinMax.x);
         }
         else
         {
-            m_MainCamera.fieldOfView += m_fCameraMove * m_CameraZoomSpeed * Time.deltaTime;
+            m_MainCamera.fieldOfView += m_fCameraZoom * m_CameraZoomSpeed * Time.deltaTime;
             m_MainCamera.fieldOfView = Mathf.Clamp(m_MainCamera.fieldOfView, m_CameraFovMinMax.x, m_CameraFovMinMax.y);
         }
 
@@ -139,8 +162,8 @@ public class InputMng : MonoBehaviour
 
     private void TouchCameraReset()
     {
-        m_fTouchPreMag = 0f;
-        m_v2TouchPreCenterPos.x = 0f;
-        m_v2TouchPreCenterPos.y = 0f;
+        m_fTouchPreMag = -1f;
+        m_v2TouchPreCenterPos.x = -1f;
+        m_v2TouchPreCenterPos.y = -1f;
     }
 }
