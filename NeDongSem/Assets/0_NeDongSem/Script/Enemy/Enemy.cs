a9ddfc920b111da2f7933775a1dc9a6822474fba
NeDongSem <riverhun2@gmail.com>
2023-09-14 10:17:46 +0900

    Enemy 제작 중

--- NeDongSem/Assets/0_NeDongSem/Script/Enemy/Enemy.cs
+++ NeDongSem/Assets/0_NeDongSem/Script/Enemy/Enemy.cs
@@ -4,12 +4,49 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    int iHp = 10;
+    struct EnemyInfo
+    {
+        public string strEnemyName;
+        public int iHP;
+        public int iSpeed;
+    }
+
+    EnemyInfo m_EnemyInfo;
+    int m_iTileIndex;
+    Vector3 m_v3TargetPos;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void Init()
+    {
+       
+    }
+
+    public void Set_EnemySpawn()
+    {
+        SettingInfo();
+    }
+
+    private void SettingInfo()
+    {
+        m_EnemyInfo = new EnemyInfo();
+        m_EnemyInfo.strEnemyName = "1";
+        m_EnemyInfo.iHP = int.Parse(InfoMng.Instance.Get_EnemyInfo(m_EnemyInfo.strEnemyName, "HP"));
+        m_EnemyInfo.iSpeed = int.Parse(InfoMng.Instance.Get_EnemyInfo(m_EnemyInfo.strEnemyName, "Speed"));
+    }
 
     public void hit(int _iDmg)
     {
-        iHp -= _iDmg;
-        if(iHp <= 0)
+        m_EnemyInfo.iHP -= _iDmg;
+        if(m_EnemyInfo.iHP <= 0)
         {
             transform.position = Vector3.zero;
             ObjectPoolMng.Instance.Return_PoolingObject(gameObject, "TestEnemy");
